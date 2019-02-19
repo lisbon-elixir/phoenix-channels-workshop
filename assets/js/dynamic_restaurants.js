c1d6a4665575fdadef7f178b7dddd2b9bd84e76a
Daniel José Domingos Serrano <danieljdserrano@gmail.com>
2019-02-19 15:17:24 +0000

    Step 6. Dynamic restaurants map via WebSockets

--- assets/js/dynamic_restaurants.js
+++ assets/js/dynamic_restaurants.js
@@ -2,8 +2,23 @@ import {channel} from "./socket"
 
 let DynamicRestaurants = {
   init() {
-    console.log("Init DynamicRestaurants")
+    this.getRestaurants(window.mymap.getCenter())
   },
+
+  getRestaurants(coords) {
+    channel.push("get_restaurants", {coords: coords}).receive(
+      "ok", ({restaurants}) => {
+        restaurants.forEach(r => {
+          DynamicRestaurants.renderRestaurant(r.latitude, r.longitude, r.name, r.url)
+        });
+      }
+    )
+  },
+
+  renderRestaurant(lat, long, name, url) {
+    const marker = window.L.marker(window.L.latLng(lat, long)).addTo(window.mymap);
+    marker.bindPopup(`<a href="${url}">${name}</a></br>`)
+  }
 }
 
 export default DynamicRestaurants
--- lib/restaurants_web/channels/user_channel.ex
+++ lib/restaurants_web/channels/user_channel.ex
@@ -6,4 +6,23 @@ defmodule RestaurantsWeb.UserChannel do
     Logger.info("Receiving connection for user #{user_id}")
     {:ok, socket}
   end
+
+  def handle_in("get_restaurants", %{"coords" => %{"lat" => lat, "lng" => lng}}, socket) do
+    {:ok, %{"nearby_restaurants" => restaurants}} = Tomato.geocode(lat, lng)
+
+    response =
+      restaurants
+      |> Enum.map(&render_restaurant/1)
+
+    {:reply, {:ok, %{restaurants: response}}, socket}
+  end
+
+  def render_restaurant(%{"restaurant" => restaurant}) do
+    %{
+      name: restaurant["name"],
+      url: restaurant["url"],
+      latitude: restaurant["location"]["latitude"],
+      longitude: restaurant["location"]["longitude"]
+    }
+  end
 end
